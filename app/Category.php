6f57d71961a677193d2bafd409ce99cdbb6a8d5e
jhongmez <gmezjhon@gmail.com>
2020-04-02 21:03:01 -0500

    MV0004 Configuration of model categorywith relation to table posts

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    //Create property protected
+
+    //Llamamos la tabla de nuestra BD
+    protected $table = 'categories';
+
+    //Creamos una funcion donde nos haga la relacion de 1 a muchos con hasMany
+    public function posts() {
+        return $this->hasMany('App\Post');
+    }
 }
