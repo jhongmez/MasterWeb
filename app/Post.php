0672fd2941e262b4aa69d499344eb1acf14c6f39
jhongmez <gmezjhon@gmail.com>
2020-04-02 21:52:35 -0500

    [MV0006] Configuration model post an relation with table users and categories

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //Llamamos la table
+    protected $table = 'posts';
+
+    //Relacion entre user y category de muchos a uno
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
 }
